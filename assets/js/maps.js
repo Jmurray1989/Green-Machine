be00450501334471e1c7fce28d3da7e0547297dd
John Murray <jmurray1989@hotmail.com>
2020-03-22 19:40:14 +0000

    added js folder and map.js file

---
+++ assets/js/maps.js
@@ -0,0 +1,27 @@
+/*Renders the map*/
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 3,
+        center: {
+            lat: 46.619261,
+            lng: -33.134766
+        }
+    });
+
+    /*Adds a marker for my location*/
+
+    var labels = "A";
+
+    var locations = [
+        { lat: 53.349957, lng: -6.260306 }, 
+    ];
+
+    var markers = locations.map(function(location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
+
+    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+}
