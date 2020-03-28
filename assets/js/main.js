f6c1acdf68c5936faf878a79ed69df80386bbc31
John Murray <jmurray1989@hotmail.com>
2020-03-28 17:04:02 +0000

    Added smooth scroll feature to Home & Skoot route sections, + Arrow button to return home

---
+++ assets/js/main.js
@@ -0,0 +1,58 @@
+
+/*Adds smooth scroll feature to nav links using jquery*/
+$('nav li').click(function(event){
+    
+    var target = $(this).find('a').attr('href');
+    smoothScroll(target);
+
+});
+
+function smoothScroll( target ) {
+    $('body, html').animate({
+        scrollTop: $(target).offset().top
+    }, 2000);
+}
+
+/*Clicking on the Arrow will return you HOME*/
+$(window).scroll(function() {
+    var height = $(window).scrollTop();
+    if (height > 100) {
+        $('#back2Top').fadeIn();
+    } else {
+        $('#back2Top').fadeOut();
+    }
+});
+$(document).ready(function() {
+    $("#back2Top").click(function(event) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: 0 }, 2000);
+        return false;
+    });
+
+});
+
+/*Renders the map & zooms in on location*/
+var map, infoWindow;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 53.349957, lng: -6.260306},
+          zoom: 14
+        });
+
+    /*Adds a marker for my location*/
+
+    var labels = "A";
+
+    var locations = [
+        { lat: 53.349957, lng: -6.260306 }, 
+    ];
+
+    var markers = locations.map(function(location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
+
+    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+}
